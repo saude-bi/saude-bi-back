e0e2d5cd35e3658bd2f313f6940baaeebe8330c8
Helcio <helcio@carmo.in>
2023-04-05 18:37:54 -0300

    feat: Add controller returning geojson

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,26 @@
-import { Controller } from '@nestjs/common'
-import { ApiTags } from '@nestjs/swagger'
+import { HttpService } from '@nestjs/axios';
+import { Controller, Get } from '@nestjs/common';
+import { Agent } from 'https';
+import { firstValueFrom } from 'rxjs';
+import { AppService } from './app.service';
 
 @Controller()
-@ApiTags('App')
-export class AppController {}
+export class AppController {
+  constructor(
+    private readonly appService: AppService,
+    private readonly httpService: HttpService,
+  ) {}
+
+  @Get()
+  async getHello(): Promise<any> {
+    const key = 'd2de988d-cbb5-4177-85fc-251164c3d26f';
+
+    return (
+      await firstValueFrom(
+        this.httpService.get(
+          'https://gist.githubusercontent.com/gustavocwl/8951b36a534c647370a4df6e09330b49/raw/8a3a9bac878a03f2bcb278bf3fbe9e15257edb1e/esgoto_foz',
+        ),
+      )
+    ).data;
+  }
+}
