6367691f8f03a076d18802f3ff72d031eb272f24
Pedro Henrique Garcia <pedro@projetodigital.com.br>
2023-04-07 14:13:19 -0300

    feat: add casl config

---
+++ src/modules/identity/auth/casl/casl-ability.factory.ts
@@ -0,0 +1,35 @@
+import {
+  AbilityBuilder,
+  AbilityClass,
+  ExtractSubjectType,
+  MatchConditions,
+  PureAbility
+} from '@casl/ability'
+import { User } from '@modules/identity/user/entities/user.entity'
+import { Injectable } from '@nestjs/common'
+import { Action, AppAbility, Subjects } from './types'
+
+const lambdaMatcher = (matchConditions: MatchConditions) => matchConditions
+
+@Injectable()
+export class CaslAbilityFactory {
+  createForUser(user: User) {
+    const { can, cannot, build } = new AbilityBuilder<AppAbility>(
+      PureAbility as AbilityClass<AppAbility>
+    )
+
+    console.log(user)
+    if (user.isAdmin) {
+      can(Action.Manage, 'all')
+    } else {
+      cannot(Action.Read, 'all')
+    }
+
+    can(Action.Read, User, ({ username }) => username === user.username)
+
+    return build({
+      detectSubjectType: (item) => item.constructor as unknown as ExtractSubjectType<Subjects>,
+      conditionsMatcher: lambdaMatcher
+    })
+  }
+}
---
+++ src/modules/identity/auth/casl/check-policies.decorator.ts
@@ -0,0 +1,58 @@
+import { RequestWithUser } from '@modules/identity/types'
+import { UserService } from '@modules/identity/user/user.service'
+import { CanActivate, ExecutionContext, Injectable, SetMetadata } from '@nestjs/common'
+import { Reflector } from '@nestjs/core'
+import { CaslAbilityFactory } from './casl-ability.factory'
+import { AppAbility } from './types'
+
+interface IPolicyHandler {
+  handle(ability: AppAbility, params: Record<string, any>): boolean
+}
+
+type PolicyHandlerCallback = IPolicyHandler['handle']
+export type PolicyHandler = IPolicyHandler | PolicyHandlerCallback
+
+export const CHECK_POLICIES_KEY = 'check_policy'
+export const CheckPolicies = (handler: PolicyHandler) => SetMetadata(CHECK_POLICIES_KEY, handler)
+
+@Injectable()
+export class PoliciesGuard implements CanActivate {
+  constructor(
+    private readonly reflector: Reflector,
+    private readonly caslAbilityFactory: CaslAbilityFactory,
+    private readonly userService: UserService
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const policyHandler = this.reflector.get<PolicyHandler>(
+      CHECK_POLICIES_KEY,
+      context.getHandler()
+    )
+
+    if (!policyHandler) {
+      return false
+    }
+
+    const params = context.getArgByIndex(0).params
+    const request: RequestWithUser = context.switchToHttp().getRequest()
+    const user = await this.userService.findOne(request.user.username)
+
+    if (!user) {
+      return false
+    }
+
+    const ability = this.caslAbilityFactory.createForUser(user)
+    return this.execPolicyHandler(policyHandler, ability, params)
+  }
+
+  private execPolicyHandler(
+    handler: PolicyHandler,
+    ability: AppAbility,
+    params: Record<string, any>
+  ) {
+    if (typeof handler === 'function') {
+      return handler(ability, params)
+    }
+    return handler.handle(ability, params)
+  }
+}
