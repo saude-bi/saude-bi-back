33ebb2c7c8aae7a5bcc75159e8791fd2001c2712
Pedro Henrique Garcia <pedro@projetodigital.com.br>
2023-06-08 20:44:52 -0300

    fix: import user service in medical worker module

--- src/modules/health/medical-worker/medical-worker.module.ts
+++ src/modules/health/medical-worker/medical-worker.module.ts
@@ -3,9 +3,10 @@ import { MedicalWorkerService } from './medical-worker.service'
 import { MedicalWorkerController } from './medical-worker.controller'
 import { MikroOrmModule } from '@mikro-orm/nestjs'
 import { MedicalWorker } from './entities/medical-worker.entity'
+import { UserModule } from '@modules/identity/user/user.module'
 
 @Module({
-  imports: [MikroOrmModule.forFeature([MedicalWorker])],
+  imports: [MikroOrmModule.forFeature([MedicalWorker]), UserModule],
   controllers: [MedicalWorkerController],
   providers: [MedicalWorkerService]
 })
