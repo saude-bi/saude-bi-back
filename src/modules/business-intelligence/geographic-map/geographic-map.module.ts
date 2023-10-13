74dd8d717fc6f0fa7040640545c531d88938d4d5
Pedro Henrique Garcia <pedro@projetodigital.com.br>
2023-10-13 19:26:01 -0300

    fix: bad imports

--- src/modules/business-intelligence/geographic-map/geographic-map.module.ts
+++ src/modules/business-intelligence/geographic-map/geographic-map.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common'
-import { GeographicDataSourceService } from './geographic-data-source.service'
-import { GeographicDataSourceController } from './geographic-data-source.controller'
-import { GeographicDataSource } from './entities/geographic-data-source.entity'
+import { GeographicMapService } from './geographic-map.service'
+import { GeographicMapController } from './geographic-map.controller'
+import { GeographicMap } from './entities/geographic-map.entity'
 import { MikroOrmModule } from '@mikro-orm/nestjs'
 
 @Module({
-  imports: [MikroOrmModule.forFeature([GeographicDataSource])],
-  controllers: [GeographicDataSourceController],
-  providers: [GeographicDataSourceService],
-  exports: [GeographicDataSourceService]
+  imports: [MikroOrmModule.forFeature([GeographicMap])],
+  controllers: [GeographicMapController],
+  providers: [GeographicMapService],
+  exports: [GeographicMapService]
 })
-export class GeographicDataSourceModule {}
+export class GeographicMapModule {}
