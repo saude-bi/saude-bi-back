24f14cb88e1f62b0e2f0a1f7988e02dde0f5feb5
helcio <helcio@carmo.in>
2023-10-18 20:48:39 -0300

    fix: Corrige relação many to many Geographic-data

--- src/modules/business-intelligence/geographic-data-source/entities/geographic-data-source.entity.ts
+++ src/modules/business-intelligence/geographic-data-source/entities/geographic-data-source.entity.ts
@@ -11,7 +11,7 @@ export class GeographicDataSource extends AuditedEntity {
   @Property()
   sourceUrl: string
 
-  @ManyToMany(() => GeographicMap, "layers")
+  @ManyToMany(() => GeographicMap, "layers", { owner: true })
   usedIn = new Collection<GeographicMap>(this)
 
   @Property()
