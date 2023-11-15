85e18b32edbfb165abc0b99aae984d699d945d18
Pedro Henrique Garcia <pedro@projetodigital.com.br>
2023-11-15 14:36:11 -0300

    fix: create geographic data source missing fields in dto

--- src/modules/business-intelligence/geographic-data-source/dto/create-geographic-data-source.dto.ts
+++ src/modules/business-intelligence/geographic-data-source/dto/create-geographic-data-source.dto.ts
@@ -1,8 +1,12 @@
-import { IsNumber, IsString } from 'class-validator'
+import { IsNumber, IsString, IsUrl } from 'class-validator'
 
 export class CreateGeographicDataSourceDto {
   @IsNumber()
   category: number
+
+  @IsString()
+  @IsUrl()
+  sourceUrl: string
 
   @IsString()
   name: string
