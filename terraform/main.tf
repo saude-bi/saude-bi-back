c9b7d3810baad36196d1deaa0fad833ed62e9709
JeanClay <83995743+JeanClay@users.noreply.github.com>
2023-11-23 21:31:47 -0300

    Feat : Adiciona o Terraform

---
+++ terraform/main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region     = "us-east-1"  # Substitua pela sua região
+  access_key = ""
+  secret_key = ""
+  token      = ""
+}
+
+resource "aws_instance" "ec2_instance" {
+  ami = data.aws_ami.ubuntu.id
+  count = "${var.number_of_instances}"
+  instance_type = "${var.instance_type}"
+  key_name = aws_key_pair.deployer.key_name
+}
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = ""
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+# You can add more variables here as needed
+
+variable "instance_name" {
+        description = "Name of the instance to be created"
+        default = "saude-bi-backend"
+}
+
+variable "instance_type" {
+        default = "t2.small"
+}
+
+variable "number_of_instances" {
+        description = "number of instances to be created"
+        default = 1
+}
+
+variable "ami_key_pair_name" {
+        default = "tomcat"
+}
