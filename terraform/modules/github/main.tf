6c58d59a4bfa1662e2a52856e27be6000e2d5002
helcio laurentino do carmo junior <helcio@carmo.in>
2023-12-07 22:18:21 -0300

    ci: change repository to id

--- terraform/modules/github/main.tf
+++ terraform/modules/github/main.tf
@@ -4,13 +4,13 @@ data "github_repository" "repo" {
 
 resource "github_repository_environment" "backend_env" {
 
-  repository       = data.github_repository.repo.name
+  repository       = data.github_repository.repo.id
   environment      = "deploy-ci"
 }
 
 resource "github_actions_environment_secret" "test_secret" {
 
-  repository       = data.github_repository.repo.name
+  repository       = data.github_repository.repo.id
   environment      = github_repository_environment.backend_env.environment
   secret_name      = "test_secret_name"
   plaintext_value  = var.public_ip
